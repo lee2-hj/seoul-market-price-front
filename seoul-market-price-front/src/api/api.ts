6bbe5297d8c32f30031ef523190c3c0ce50f9c16
lee2-hj <79687059+lee2-hj@users.noreply.github.com>
2026-07-31 15:19:51 +0900

    🔧 회원 아이디 중복체크 api 수정

--- seoul-market-price-front/src/api/api.ts
+++ seoul-market-price-front/src/api/api.ts
@@ -131,7 +131,7 @@ export async function checkUserIdApi(
     const response =
         await apiMiddleware.get(
 
-            "/api/users/check-id",
+            "/api/members/check-id",
 
             {
 
